c4155f313434ccab38fe978a42044d79efa109b9
Camile Coelho <camilecoelho23@gmail.com>
2023-11-14 17:22:17 -0300

    Implementado serviços mais básico.

---
+++ eAgendaMedica.Aplicacao/ModuloAtividade/ServicoCirurgia.cs
@@ -0,0 +1,89 @@
+﻿using eAgendaMedica.Dominio.Compartilhado;
+using eAgendaMedica.Dominio.ModuloAtividade;
+
+namespace eAgendaMedica.Aplicacao.ModuloAtividade
+{
+    public class ServicoCirurgia
+    {
+        private readonly IRepositorioCirurgia repositorioCirurgia;
+        private readonly IContextoPersistencia contextoPersistencia;
+
+        public ServicoCirurgia(IRepositorioCirurgia repositorioCirurgia, IContextoPersistencia contextoPersistencia)
+        {
+            this.repositorioCirurgia = repositorioCirurgia;
+            this.contextoPersistencia = contextoPersistencia;
+        }
+
+
+        public async Task<Result<Cirurgia>> InserirAsync(Cirurgia cirurgia)
+        {
+            var resultadoValidacao = ValidarCirurgia(cirurgia);
+
+            if (resultadoValidacao.IsFailed)
+                return Result.Fail(resultadoValidacao.Errors);
+
+            await repositorioCirurgia.InserirAsync(cirurgia);
+
+            await contextoPersistencia.GravarDadosAsync();
+
+            return Result.Ok(cirurgia);
+        }
+
+        public async Task<Result<Cirurgia>> EditarAsync(Cirurgia cirurgia)
+        {
+            var resultadoValidacao = ValidarCirurgia(cirurgia);
+
+            if (resultadoValidacao.IsFailed)
+                return Result.Fail(resultadoValidacao.Errors);
+
+            repositorioCirurgia.Editar(cirurgia);
+
+            await contextoPersistencia.GravarDadosAsync();
+
+            return Result.Ok(cirurgia);
+        }
+
+        public async Task<Result<Cirurgia>> ExcluirAsync(Guid id)
+        {
+            var cirurgia = await repositorioCirurgia.SelecionarPorIdAsync(id);
+
+            repositorioCirurgia.Excluir(cirurgia);
+
+            await contextoPersistencia.GravarDadosAsync();
+
+            return Result.Ok();
+        }
+
+        public async Task<Result<List<Cirurgia>>> SelecionarTodosAsync()
+        {
+            var cirurgias = await repositorioCirurgia.SelecionarTodosAsync();
+
+            return Result.Ok(cirurgias);
+        }
+
+        public async Task<Result<Cirurgia>> SelecionarPorIdAsync(Guid id)
+        {
+            var cirurgia = await repositorioCirurgia.SelecionarPorIdAsync(id);
+
+            return Result.Ok(cirurgia);
+        }
+
+
+        private Result ValidarCirurgia(Cirurgia cirurgia)
+        {
+            ValidadorCirurgia validador = new ValidadorCirurgia();
+
+            var resultadoValidacao = validador.Validate(cirurgia);
+
+            List<Error> erros = new List<Error>();
+
+            foreach (var erro in resultadoValidacao.Errors)
+                erros.Add(new Error(erro.ErrorMessage));
+
+            if (erros.Any())
+                return Result.Fail(erros.ToArray());
+
+            return Result.Ok();
+        }
+    }
+}
---
+++ eAgendaMedica.Aplicacao/ModuloAtividade/ServicoConsulta.cs
@@ -0,0 +1,88 @@
+﻿using eAgendaMedica.Dominio.ModuloAtividade;
+
+namespace eAgendaMedica.Aplicacao.ModuloAtividade
+{
+    public class ServicoConsulta
+    {
+        private readonly IRepositorioConsulta repositorioConsulta;
+        private readonly IContextoPersistencia contextoPersistencia;
+
+        public ServicoConsulta(IRepositorioConsulta repositorioConsulta, IContextoPersistencia contextoPersistencia)
+        {
+            this.repositorioConsulta = repositorioConsulta;
+            this.contextoPersistencia = contextoPersistencia;
+        }
+
+
+        public async Task<Result<Consulta>> InserirAsync(Consulta consulta)
+        {
+            var resultadoValidacao = ValidarConsulta(consulta);
+
+            if (resultadoValidacao.IsFailed)
+                return Result.Fail(resultadoValidacao.Errors);
+
+            await repositorioConsulta.InserirAsync(consulta);
+
+            await contextoPersistencia.GravarDadosAsync();
+
+            return Result.Ok(consulta);
+        }
+
+        public async Task<Result<Consulta>> EditarAsync(Consulta consulta)
+        {
+            var resultadoValidacao = ValidarConsulta(consulta);
+
+            if (resultadoValidacao.IsFailed)
+                return Result.Fail(resultadoValidacao.Errors);
+
+            repositorioConsulta.Editar(consulta);
+
+            await contextoPersistencia.GravarDadosAsync();
+
+            return Result.Ok(consulta);
+        }
+
+        public async Task<Result<Consulta>> ExcluirAsync(Guid id)
+        {
+            var consulta = await repositorioConsulta.SelecionarPorIdAsync(id);
+
+            repositorioConsulta.Excluir(consulta);
+
+            await contextoPersistencia.GravarDadosAsync();
+
+            return Result.Ok();
+        }
+
+        public async Task<Result<List<Consulta>>> SelecionarTodosAsync()
+        {
+            var consultas = await repositorioConsulta.SelecionarTodosAsync();
+
+            return Result.Ok(consultas);
+        }
+
+        public async Task<Result<Consulta>> SelecionarPorIdAsync(Guid id)
+        {
+            var consulta = await repositorioConsulta.SelecionarPorIdAsync(id);
+
+            return Result.Ok(consulta);
+        }
+
+
+        private Result ValidarConsulta(Consulta consulta)
+        {
+            ValidadorConsulta validador = new ValidadorConsulta();
+
+            var resultadoValidacao = validador.Validate(consulta);
+
+            List<Error> erros = new List<Error>();
+
+            foreach (var erro in resultadoValidacao.Errors)
+                erros.Add(new Error(erro.ErrorMessage));
+
+            if (erros.Any())
+                return Result.Fail(erros.ToArray());
+
+            return Result.Ok();
+        }
+    }
+}
