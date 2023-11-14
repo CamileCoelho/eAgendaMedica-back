f6507de337cd51ce08873659e1ebb3a12ec4639e
Camile Coelho <camilecoelho23@gmail.com>
2023-11-14 15:00:27 -0300

    Adicionado mensagem as validações de horario.

--- eAgendaMedica.Dominio/ModuloAtividade/ValidadorCirurgia.cs
+++ eAgendaMedica.Dominio/ModuloAtividade/ValidadorCirurgia.cs
@@ -15,7 +15,8 @@
             RuleFor(x => x.HoraTermino)
                 .NotNull()
                 .NotEmpty()
-                .GreaterThan((x) => x.HoraInicio);
+                .GreaterThan((x) => x.HoraInicio)
+                .WithMessage("Horário de término deve ser posterior ao horário de início.");
 
             RuleFor(x => x.PeriodoRecuperacao)
                 .Equal(TimeSpan.FromMinutes(240));
